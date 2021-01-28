e0b06789c49039b7d32f125ca1098d017d12ce7b
slenane <slenane1@gmail.com>
2021-01-28 22:36:17 +0000

    Added Bootstrap and organised files

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
-const Treatment = require("./models/treatment");
+// MODELS
 const Jewellery = require("./models/jewellery");
 const Blog = require("./models/blog");
 
@@ -23,6 +24,7 @@ db.once("open", () => {
 const app = express();
 
 // APP SETTINGS
+app.engine('ejs', ejsMate)
 app.set("view engine", "ejs");
 app.set('views', path.join(__dirname, 'views'))
 
@@ -37,17 +39,17 @@ app.get('/', (req, res) => {
 // TREATMENT ROUTES
 // INDEX
 app.get("/treatments", (req, res) => {
-    res.render("treatments/index");
+    res.render("sections/treatments/index");
 });
 
 // SHOW - ENERGY TREATMENT
 app.get("/treatments/energy-treatment", (req, res) => {
-    res.render("treatments/energy-treatment/show");
+    res.render("sections/treatments/energy-treatment/show");
 });
 
 // SHOW - ABSENTEE TREATMENT
 app.get("/treatments/absentee-treatment", (req, res) => {
-    res.render("treatments/absentee-treatment/show");
+    res.render("sections/treatments/absentee-treatment/show");
 });
 
 
@@ -55,42 +57,72 @@ app.get("/treatments/absentee-treatment", (req, res) => {
 // INDEX
 app.get("/jewellery", async (req, res) => {
     const jewelleries = await Jewellery.find({}) || [];
-    res.render("jewellery/index", { jewelleries });
+    res.render("sections/jewellery/index", { jewelleries });
 });
 
 // SHOW
 app.get('/jewellery/:id', async (req, res,) => {
     const jewellery = await Jewellery.findById(req.params.id)
-    res.render('jewellery/show', { jewellery });
+    res.render('sections/jewellery/show', { jewellery });
 });
 
 
 // BLOG ROUTES
 // INDEX
 app.get("/blogs", async (req, res) => {
-    const blogs = await Blog.find({}) || [];
-    res.render("blogs/index", { blogs });
+    const blogs = await Blog.find({});
+    res.render("sections/blogs/index", { blogs });
+});
+
+// NEW
+app.get("/blogs/new", (req, res) => {
+    res.render("sections/blogs/new");
+});
+
+app.post("/blogs", async (req, res) => {
+    const blog = new Blog(req.body.blog);
+    await blog.save();
+    res.redirect(`/blogs/${blog._id}`)
 });
 
 // SHOW
 app.get('/blogs/:id', async (req, res,) => {
     const blog = await Blog.findById(req.params.id)
-    res.render('blogs/show', { blog });
+    res.render('sections/blogs/show', { blog });
 });
+
+// EDIT 
+app.get('/blogs/:id/edit', async (req, res) => {
+    const blog = await Blog.findById(req.params.id)
+    res.render('sections/blogs/edit', { blog });
+})
+
+app.put('/blogs/:id', async (req, res) => {
+    const { id } = req.params;
+    const blog = await Blog.findByIdAndUpdate(id, { ...req.body.blog });
+    res.redirect(`/blogs/${blog._id}`)
+});
+
+// DELETE
+app.delete('/blogs/:id', async (req, res) => {
+    const { id } = req.params;
+    await Blog.findByIdAndDelete(id);
+    res.redirect('/blogs');
+})
 
 // AWARENESS ROUTE
 app.get("/awareness", (req, res) => {
-    res.render("awareness/index");
+    res.render("sections/awareness/index");
 });
 
 // ABOUT ROUTE
 app.get("/about", (req, res) => {
-    res.render("about/index");
+    res.render("sections/about/index");
 });
 
 // CONTACT ROUTE
 app.get("/contact", (req, res) => {
-    res.render("contact/index");
+    res.render("sections/contact/index");
 });
 
 // LISTENING
