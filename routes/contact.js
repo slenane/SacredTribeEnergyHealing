724189ffca13fc9b47ccb48c639be329a8326fbd
slenane <slenane1@gmail.com>
2021-03-11 14:16:59 +0000

    Added list of materials and images

--- routes/contact.js
+++ routes/contact.js
@@ -22,7 +22,7 @@ transporter.verify(function (error, success) {
     if (error) {
         console.log(error);
     } else {
-        console.log("Server is ready to take our messages");
+        console.log("Nodemailer running");
     }
 });
 
