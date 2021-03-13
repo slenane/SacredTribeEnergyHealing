b970e7411a81d67e343f5eb5c8d210b570b4d3ad
slenane <slenane1@gmail.com>
2021-03-13 20:42:56 +0000

    Added jewllery index media queries

--- public/javascripts/jewellery.js
+++ public/javascripts/jewellery.js
@@ -1,32 +1,60 @@
-let heroDiv = document.querySelector("#jewellery__hero_div");
+let infobar = document.querySelector(".hero_infobar");
 let infobarText = document.querySelector(".hero_infobar--text");
 let buyingGuide = document.querySelector(".hero_infobar--grid");
-let buyingGuideBtn = document.querySelector(".buying_guide-button")
-let heroGrid = document.querySelector(".hero_grid");
+let buyingGuideBtn = document.querySelector(".buying_guide-button");
 
-let toggleShowDropdown = (e) => {
+let toggleShowDropdown = async (e) => {
     if (buyingGuide.classList.contains("hide")) {
-        // Adjust the CSS grid to have space for the dropdown menu
-        heroDiv.style.gridTemplateRows = "55% 50% 35%";
-        // Remove the infobar text
+        // Remove the infobar text and button
         infobarText.classList.add("hide");
+        buyingGuideBtn.classList.add("hide");
         // Show the buying guide
-        buyingGuide.classList.remove("hide");
-        // Change the text content of the button
-        buyingGuideBtn.textContent = "Read Less";
-        // Hide the grid below the buying guide when dropdown open
-        heroGrid.classList.add("hide");
+        buyingGuide.classList.remove("hide"); 
+        buyingGuide.classList.remove("fade-out");
+        buyingGuide.classList.add("fade-in");
+        setTimeout(() => {
+            buyingGuide.classList.add("active");  
+        }, 1000);
+
+        // Resize the infobar
+        infobar.classList.remove("dropup"); 
+        infobar.classList.add("dropdown");
+        // Add an active class to set the new div height 
+        setTimeout(() => {
+            infobar.classList.add("active");  
+
+            // Show and change the text content of the button
+            buyingGuideBtn.classList.remove("hide");
+            buyingGuideBtn.textContent = "Read Less";
+        }, 500);
+
+
     } else {
-        // Return grid to orignal size
-        heroDiv.style.gridTemplateRows = "55% 10% 35%";
-        // Display infobar text
-        infobarText.classList.remove("hide");
+        // Hide the button 
+        buyingGuideBtn.classList.add("hide");
+
         // Hide the buying guide
-        buyingGuide.classList.add("hide");
-        // Change the text content of the button
-        buyingGuideBtn.textContent = "Read More";
-        // Show the grid below the buying guide when dropdown closed
-        heroGrid.classList.remove("hide");
+        buyingGuide.classList.remove("fade-in");
+        buyingGuide.classList.add("fade-out");
+        setTimeout(() => {
+            buyingGuide.classList.remove("active");
+            buyingGuide.classList.add("hide");   
+
+            // Display infobar text
+            infobarText.classList.remove("hide");
+        }, 500);
+
+        // Resize the infobar
+        infobar.classList.remove("dropdown"); 
+        infobar.classList.add("dropup"); 
+        // Remove active class to set the div height to initial
+        setTimeout(() => {
+            infobar.classList.remove("active");  
+
+            // Show and change the text content of the button
+            buyingGuideBtn.classList.remove("hide");
+            buyingGuideBtn.textContent = "Read More";
+        }, 500);
     }
 };
 
